107b8e19e8b52bba14b5348a40a762520b8f3b8a
Bikembe <bikembecs@gmail.com>
2025-07-11 18:48:01 -0600

    Actualización de cambios

--- artesanias/productos/views.py
+++ artesanias/productos/views.py
@@ -3,20 +3,29 @@ from .models import Producto, Filtro
 
 def detalle_producto(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
-    return render(request, 'productos/detalle.html', {'producto': producto})
+    filtros = Filtro.objects.all()
+    filtro_slug = request.GET.get('filtro')
+
+    mas_productos = Producto.objects.exclude(pk=pk)[:4]
+
+    return render(request, 'productos/detalle.html', {
+        'producto': producto,
+        'filtros': filtros,
+        'filtro_seleccionado': filtro_slug,
+        'mas_productos': mas_productos,
+    })
 
 def lista_productos(request):
     filtro_slug = request.GET.get('filtro')
     filtros = Filtro.objects.all()
     productos = Producto.objects.all()
-    print('Filtro recibido:', filtro_slug)  # Para debug
 
     if filtro_slug:
         try:
             filtro_obj = Filtro.objects.get(slug=filtro_slug)
             productos = productos.filter(filtro=filtro_obj)
         except Filtro.DoesNotExist:
-            productos = Producto.objects.none()  # No hay filtro, no mostrar nada o todos
+            productos = Producto.objects.none()
 
     return render(request, 'productos/lista.html', {
         'productos': productos,
